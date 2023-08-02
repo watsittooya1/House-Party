1c3478db5a2b2820a065f57393ca48a7c5be5cfe
Chris <watsittooya1@gmail.com>
2023-08-02 16:26:35 -0400

    added styling, home/join/create page components, react router

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+
 
 export default class App extends Component { 
     constructor(props) {
@@ -8,7 +10,8 @@ export default class App extends Component {
     }
 
     render() {
-        return <h1>Testing React Code</h1>;
+
+        return <HomePage />;
     }
 }
 
---
+++ music_controller/frontend/src/components/HomePage.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter as Router, Routes, Route, Link, Redirect } from "react-router-dom";
+
+
+export default class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<p>This is the home page</p>}/>
+                    <Route exact path="/join" element={<RoomJoinPage/>}/>
+                    <Route exact path="/create" element={<CreateRoomPage/>}/>
+                </Routes>
+            </Router>
+        );
+    }
+}
