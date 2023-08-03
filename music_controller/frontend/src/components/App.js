73a4158dd2739d7ea0f4c3295fc3ed36ede26d80
Chris <watsittooya1@gmail.com>
2023-08-03 18:41:16 -0400

    added room join page, with API endpoint

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -10,8 +10,11 @@ export default class App extends Component {
     }
 
     render() {
-
-        return <HomePage />;
+        return (
+            <div className="center">
+                <HomePage />
+            </div>
+        );
     }
 }
 
