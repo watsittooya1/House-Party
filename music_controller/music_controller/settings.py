607699eff058b9afb1faa07f9a1b462f27080850
Chris <watsittooya1@gmail.com>
2023-09-29 10:54:20 -0400

    changed secure cross origin opener policy setting

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -129,3 +129,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = None
