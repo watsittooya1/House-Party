f056759453692f6e1137e88115dfebc57e581ab8
Chris <watsittooya1@gmail.com>
2023-09-29 10:58:16 -0400

    added server as allowed host

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '***REMOVED***'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '10.0.0.21']
+ALLOWED_HOSTS = ['127.0.0.1', '10.0.0.21', '142.93.184.143']
 
 
 # Application definition
