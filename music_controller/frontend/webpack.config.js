fbba130e9396da3c6eeef733f011ba7e2eddade2
Chris <watsittooya1@gmail.com>
2023-10-02 17:28:58 -0400

    testing with webpack publicpath

--- music_controller/frontend/webpack.config.js
+++ music_controller/frontend/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
-    publicPath: "/static/",
+    publicPath: "/static",
     filename: "[name].js",
   },
   module: {
