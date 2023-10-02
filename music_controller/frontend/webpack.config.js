4ea6ef70404322d4ed005c72fe51085cfc736bd2
Chris <watsittooya1@gmail.com>
2023-10-02 17:34:59 -0400

    changed webpack public path to static.frontend

--- music_controller/frontend/webpack.config.js
+++ music_controller/frontend/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
-    publicPath: "/static",
+    publicPath: "/static/frontend",
     filename: "[name].js",
   },
   module: {
