5f5ff0a510a6c8827d7286e852bbe2e9b1f34317
Chris <watsittooya1@gmail.com>
2023-10-10 22:01:58 -0400

    removed

--- music_controller/spotify/credentials.py
+++ music_controller/spotify/credentials.py
@@ -1,4 +1,4 @@
 # these variables are typically stored via environment variables
-CLIENT_ID = "***REMOVED***"
-CLIENT_SECRET = "***REMOVED***"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+#CLIENT_ID = ""
+#CLIENT_SECRET = ""
+#REDIRECT_URI = ""
